3470a14daf190563a12930c8df9a98057f01b5dd
tuanhoangle <tuanleprofessional@gmail.com>
2021-03-09 22:20:56 -0500

    lot of changes

--- public/assets/js/experiences.js
+++ public/assets/js/experiences.js
@@ -1,21 +1,18 @@
 $(document).ready(function () {
   $.getJSON("public/assets/data/experience_list_data.json", function (data) {
     data = data.experience_list;
-    var container = $("<div>").attr("class", "container");
+    var container = $("<div>").attr("class", "container-fluid");
 
     for (var i = 0; i < data.length; i++) {
-      var row = $("<div>").attr("class", "row");
+      var row = $("<div>").attr("class", "row-fluid");
       row.attr("style", "padding: 20px 0px 20px 0px");
-      var col1 = $("<div>").attr("class", "col-4");
-      var col2 = $("<div>").attr("class", "col-8");
+      var col1 = $("<div>").attr("class", "resume_left");
+      var col2 = $("<div>").attr("class", "resume_right");
 
       var title = $("<h4>").text(data[i].title);
       var date = $("<p>").text(data[i].date);
       var description = $("<p>").text(data[i].description);
-      description.attr("style", "text-align: left;");
       var skill = data[i].skill;
-
-      console.log(data[i].name);
 
       row.append(col1, col2);
       col1.append(title, date);
--- script.js
+++ script.js
@@ -1,39 +1,30 @@
-$(document).ready(function () {
-  window.setTimeout(1000);
+function navToDiv(id) {
+  $(`#${id}`).attr("style", "color:#66fcf1");
+  $(`.nav-link`)
+    .not($(`#${id}`))
+    .attr("style", "color:white");
 
-  const nav_select = localStorage.getItem("nav-select");
-  if (nav_select != null) {
-    navToDiv(nav_select);
-  }
-  $("#nav-home").click(function () {
-    navToDiv(this.id);
+  id = id.replace("nav-", "").toLowerCase();
+  $("html,body").animate(
+    {
+      scrollTop: $(`#${id}`).offset().top,
+    },
+    "slow"
+  );
+}
+
+function render_skill_list() {
+  return new Promise((resolve, reject) => {
+    $.getJSON("./public/assets/data/skill_data.json", (data) => {
+      resolve(data);
+    });
   });
-  $("#nav-about").click(function () {
-    navToDiv(this.id);
-  });
-  $("#nav-resume").click(function () {
-    navToDiv(this.id);
-  });
+}
 
-  function navToDiv(id) {
-    $(`#${id}`).attr("style", "color:orange");
-    $(`.nav-link`)
-      .not($(`#${id}`))
-      .attr("style", "color:white");
-
-    localStorage.setItem("nav-select", id);
-
-    id = id.replace("nav-", "").toLowerCase();
-    $("html,body").animate(
-      {
-        scrollTop: $(`#${id}`).offset().top,
-      },
-      "slow"
-    );
-  }
-
-  $.getJSON("public/assets/data/skill_data.json", function (data) {
-    var skill_array = data.skill_array;
+async function init() {
+  try {
+    let data = await render_skill_list();
+    let skill_array = data.skill_array;
     skill_array.sort(function (a, b) {
       let x = a.progress.toLowerCase(),
         y = b.progress.toLowerCase();
@@ -55,5 +46,27 @@ $(document).ready(function () {
       container.append(p, div);
       $("#technical_skill").append(container);
     }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+$(document).ready(function () {
+  $("#nav-home").click(function () {
+    navToDiv(this.id);
   });
+  $("#nav-about").click(function () {
+    navToDiv(this.id);
+  });
+  $("#nav-resume").click(function () {
+    navToDiv(this.id);
+  });
+  $("#nav-project").click(function () {
+    navToDiv(this.id);
+  });
+  $("#nav-contact").click(function () {
+    navToDiv(this.id);
+  });
+
+  init();
 });
